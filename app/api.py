9b003c66a96a19853f64fdd74658d7acff7c252c
Ivan Huang <ivanhuang@Ivans-MacBook-Pro.local>
2020-05-07 09:11:11 -0500

    Standardized interface of api

--- app/api.py
+++ app/api.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import subprocess
 import time
@@ -14,10 +15,13 @@ throttler = Throttler(logger)
 @app.route('/lighthouse')
 def lighthouse():
     job_id = str(int(time.time()*1000))
-    place_in_queue = throttler.enqueue(job_id=job_id, url='http://18.222.118.221')
-    return '{}, {}'.format(job_id, place_in_queue), 200
+    throttler.enqueue(job_id=job_id, url='http://18.222.118.221')
+    return job_id, 200
 
 @app.route('/lighthouse_results/<job_id>')
 def lighthouse_results(job_id):
-    lighthouse_results = throttler.fetch(job_id)
-    return lighthouse_results
+    lighthouse_status = throttler.place_in_line(job_id=job_id)
+    if lighthouse_status['place_in_line']:
+        return json.dumps(lighthouse_status)
+    results = throttler.fetch(job_id=job_id)
+    return json.dumps(results)
--- app/classes/throttler.py
+++ app/classes/throttler.py
@@ -1,4 +1,5 @@
 import collections
+import json
 import subprocess
 import time
 
@@ -16,20 +17,37 @@ class Throttler:
 
     def enqueue(self, job_id, url):
         self.queue.append((job_id, url))
-        return len(self.queue)
+
+    def place_in_line(self, job_id):
+        place_in_line = -1
+        for i, obj in enumerate(self.queue):
+            if job_id == obj[0]:
+                place_in_line = i
+                break
+        return {'place_in_line': place_in_line+1}
 
     def fetch(self, job_id):
-        return self.results[job_id]
+        if self.results[job_id]:
+            results = self.results[job_id]
+            del self.results[job_id]
+        else:
+            results = {'status': 'pending'}
+        return results
 
     def dequeue(self):
         while True:
             self.logger.info('dequeuing')
             if self.queue:
-                job_id, url = self.queue.popleft()
-                self.logger.info('dequeued {}'.format(job_id))
-                cmd = 'lighthouse {} --output json --quiet --chrome-flags="--headless --disable-gpu --no-sandbox"'.format(url)
-                lighthouse_result = subprocess.check_output(cmd, shell=True)
-                with open('/app/reports/{}.json'.format(job_id), 'w') as file:
-                    file.write(lighthouse_result.decode('utf-8'))
-                self.results[job_id] = lighthouse_result
+
+                try:
+                    job_id, url = self.queue.popleft()
+                    self.logger.info('dequeued {}'.format(job_id))
+                    cmd = 'lighthouse {} --output json --quiet --chrome-flags="--headless --disable-gpu --no-sandbox"'.format(url)
+                    lighthouse_result = subprocess.check_output(cmd, shell=True).decode('utf-8')
+                    with open('/app/reports/{}.json'.format(job_id), 'w') as file:
+                        file.write(lighthouse_result)
+                    self.results[job_id] = {'status': 'complete', 'results': json.loads(lighthouse_result)}
+                except Exception as e:
+                    self.results[job_id] = {'status': 'error', 'message': '{}'.format(e)}
+
             time.sleep(5)
